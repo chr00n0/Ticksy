3a43f261e0e286facabf008c2495f01706d9936b
Emil Piórkowski <e.piorkowski11@gmail.com>
2026-03-13 19:27:38 +0100

    Initial commit

---
+++ Ticksy/TicksyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicksyApp.swift
+//  Ticksy
+//
+//  Created by Emil Piórkowski on 13/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct TicksyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
